9a7b583803aeba7f5bd6f2bad68d3c6e0368ba6e
steeve0304 <zychsteeve4@gmail.com>
2023-08-15 13:46:43 +0200

    ajout page 404

--- 404/script.js
+++ 404/script.js
@@ -1,87 +1,87 @@
+// Selectors
 const menuIcon = document.querySelector('.menu-icon');
 const navLinks = document.querySelector('.nav-links');
-
-menuIcon.addEventListener('click', () => {
-  document.querySelector('.navbar').classList.toggle('active');
-});
-
-
-const startButton = document.querySelector('.start-button');
-const restartButton = document.querySelector('.restart-button');
-const memoryButtons = document.querySelectorAll('.memory-button');
+const hangmanDisplay = document.getElementById('hangman-display');
+const letterInput = document.getElementById('letter-input');
+const guessButton = document.getElementById('guess-button');
 const resultMessage = document.getElementById('result-message');
-const sequence = [1, 2, 3, 4];
-let shuffledSequence = [];
-let playerSequence = [];
-let currentIndex = 0;
+const restartButton = document.getElementById('restart-button');
 
-startButton.addEventListener('click', startGame);
+// Array of words for the Hangman game
+const words = ['HELLO', 'WORLD', 'JAVASCRIPT', 'DEVELOPER', 'HANGMAN'];
+
+// Global variables
+let currentWord = '';
+let guessedLetters = [];
+let remainingAttempts = 6;
+
+// Initialize the game
+startGame();
+
+// Event listener for the "Guess" button
+guessButton.addEventListener('click', guessLetter);
+
+// Event listener for the "Restart" button
 restartButton.addEventListener('click', restartGame);
 
-memoryButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (playerSequence.length < sequence.length) {
-      const number = parseInt(button.dataset.number);
-      playerSequence.push(number);
-      checkSequence();
+// Function to start the game
+function startGame() {
+  currentWord = words[Math.floor(Math.random() * words.length)];
+  guessedLetters = [];
+  remainingAttempts = 6;
+  updateDisplay();
+}
+
+// Function to update the display
+function updateDisplay() {
+  hangmanDisplay.textContent = currentWord
+    .split('')
+    .map(letter => (guessedLetters.includes(letter) ? letter : '_'))
+    .join(' ');
+
+  resultMessage.textContent = `Remaining attempts: ${remainingAttempts}`;
+}
+
+// Function to handle letter guessing
+function guessLetter() {
+  const guessedLetter = letterInput.value.toUpperCase();
+  if (!guessedLetters.includes(guessedLetter)) {
+    guessedLetters.push(guessedLetter);
+    if (!currentWord.includes(guessedLetter)) {
+      remainingAttempts--;
     }
-  });
+  }
+  letterInput.value = '';
+  updateDisplay();
+
+  // Check for game over
+  if (remainingAttempts === 0) {
+    resultMessage.textContent = `Game over! The word was "${currentWord}"`;
+    resultMessage.style.color = '#e74c3c';
+    guessButton.disabled = true;
+  }
+
+  // Check for victory
+  if (!hangmanDisplay.textContent.includes('_')) {
+    resultMessage.textContent = `Congratulations! You guessed the word "${currentWord}"`;
+    resultMessage.style.color = '#27ae60';
+    guessButton.disabled = true;
+  }
+}
+
+// Event listener for the "Enter" key
+letterInput.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    guessLetter();
+  }
 });
 
-function startGame() {
-  shuffledSequence = shuffleArray(sequence.slice());
-  hideNumbers();
-  resultMessage.textContent = '';
-  playerSequence = [];
-  currentIndex = 0;
-  startButton.disabled = true;
-  restartButton.disabled = true;
-  setTimeout(() => {
-    revealNumbers();
-  }, 1000);
-}
-
+// Function to restart the game
 function restartGame() {
-  startButton.disabled = false;
-  restartButton.disabled = true;
-  hideNumbers();
-  resultMessage.textContent = '';
-  playerSequence = [];
-  currentIndex = 0;
+  startGame(); // function to start the game
+  guessButton.disabled = false; // Reactivate the "Guess" button
 }
 
-function checkSequence() {
-  if (playerSequence[currentIndex] === shuffledSequence[currentIndex]) {
-    currentIndex++;
-    if (currentIndex === sequence.length) {
-      resultMessage.textContent = 'Congratulations! You won the Memory Game!';
-      resultMessage.style.color = '#27ae60';
-      restartButton.disabled = false;
-    }
-  } else {
-    resultMessage.textContent = 'Oops, wrong sequence! Try again.';
-    resultMessage.style.color = '#e74c3c';
-    restartButton.disabled = false;
-  }
-}
+  
 
-function shuffleArray(array) {
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
 
-function hideNumbers() {
-  memoryButtons.forEach(button => {
-    button.textContent = '?';
-  });
-}
-
-function revealNumbers() {
-  memoryButtons.forEach((button, index) => {
-    button.textContent = shuffledSequence[index];
-  });
-}
